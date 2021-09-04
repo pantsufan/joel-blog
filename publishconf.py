e802fef8773022effb3ed52fd719ac572982d926
Pantsufan <joel28@protonmail.com>
2021-09-04 10:44:48 +0530

    conf change

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pantsufan.github.io/joel-blog'
+SITEURL = 'https://joel28.ml'
 RELATIVE_URLS = False
 FEED_MAX_ITEMS = 15
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
